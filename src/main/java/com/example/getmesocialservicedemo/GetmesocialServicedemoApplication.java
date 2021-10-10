0e835de4163c0fb40a785ec6328b13c99620523b
abhisekChowdhury <a.chowdhury94@gmail.com>
2021-10-09 22:09:35 -0700

    first commit

---
+++ src/main/java/com/example/getmesocialservicedemo/GetmesocialServicedemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.getmesocialservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetmesocialServicedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetmesocialServicedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/getmesocialservicedemo/GetmesocialServicedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getmesocialservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetmesocialServicedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
